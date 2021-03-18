194409f87a7eeaf9ecd176b062dba878e611fa7e
Jesse Stewart <jessestewart@Nels-MBP.localdomain>
2021-03-18 11:35:49 -0500

    The DOM and Type Casting

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,7 +1,18 @@
 "use strict";
-var character = 'mario';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
+// const anchor = document.querySelector('a')!
+// if (anchor) {
+//   console.log(anchor.href)
+// }
+// console.log(anchor.href)
+// const form = document.querySelector('form')!
+var form = document.querySelector('.new-item-form');
+console.log(form.children);
+// inputs 
+var type = document.querySelector('#type');
+var tofrom = document.querySelector('#tofrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,9 +1,28 @@
-const character = 'mario';
+// const anchor = document.querySelector('a')!
 
-console.log(character);
+// if (anchor) {
+//   console.log(anchor.href)
+// }
 
-const inputs = document.querySelectorAll('input');
+// console.log(anchor.href)
 
-inputs.forEach(input => {
-  console.log(input);
-});
+// const form = document.querySelector('form')!
+const form = document.querySelector('.new-item-form') as HTMLFormElement
+console.log(form.children)
+
+// inputs 
+const type = document.querySelector('#type') as HTMLSelectElement
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement
+const details = document.querySelector('#details') as HTMLInputElement
+const amount = document.querySelector('#amount') as HTMLInputElement
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault()
+
+  console.log(
+    type.value,
+    tofrom.value,
+    details.value,
+    amount.valueAsNumber,
+  )
+})
